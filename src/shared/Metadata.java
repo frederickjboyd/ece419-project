824169bb81cfd884916e5b629d8aee832a076fa5
Frederick Boyd <frederickjboyd@gmail.com>
2022-02-26 16:44:06 -0500

    Actually resolve metadata merge conflict

--- src/shared/Metadata.java
+++ src/shared/Metadata.java
@@ -1,7 +1,6 @@
 package shared;
 
 import java.math.BigInteger;
-<<<<<<< HEAD
 
 public class Metadata {
     private String host;
@@ -32,27 +31,3 @@ public class Metadata {
         return this.hashStop;
     }
 }
-=======
-import java.security.NoSuchAlgorithmException;
-import java.security.MessageDigest;
-import java.security.DigestException;
-
-public class Metadata {
-    /*
-     * set up dynamic array as metadata
-     */
-    public String address;
-    public Integer port;
-    public BigInteger start;
-    public BigInteger end;
-
-    //creating a constructor of the class that initializes the values  
-    public Metadata(String _address, Integer _port, BigInteger _start, BigInteger _end)   
-    {   
-        _address = _address;
-        _port = _port;
-        _start = _start;
-        _end = _end;   
-    }  
-}
->>>>>>> origin/m2-client-1

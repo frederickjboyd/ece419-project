ed4c8e9b24dfecd1a32f0ba9dcea68e2bfda90ff
Tijauna <danielding50@gmail.com>
2022-04-01 18:36:28 -0400

    make serverStatusInfo public in ecs client

--- src/app_kvECS/ECSClient.java
+++ src/app_kvECS/ECSClient.java
@@ -54,7 +54,7 @@ public class ECSClient implements IECSClient {
     private boolean running = false;
     private boolean zkServerRunning = false;
     private static final String PROMPT = "ECS> ";
-    private HashMap<String, NodeStatus> serverStatusInfo = new HashMap<String, NodeStatus>();
+    public HashMap<String, NodeStatus> serverStatusInfo = new HashMap<String, NodeStatus>();
     private HashRing hashRing;
     private List<String> unavailableServers = new ArrayList<String>(); // Any servers that are not OFFLINE
     private HashMap<String, Process> runningServers = new HashMap<String, Process>();

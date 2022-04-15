155db3f5c9a87b3a77057ba780abaad73f3a364e
Frederick Boyd <frederickjboyd@gmail.com>
2022-04-15 16:07:54 -0400

    Add tests for even hash ranges

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -27,6 +27,7 @@ public class AllTests {
         clientSuite.addTestSuite(MessageTest.class); // worked
         // clientSuite.addTestSuite(AdditionalTest.class); // worked
         clientSuite.addTestSuite(HashRingTest.class); // worked
+        clientSuite.addTestSuite(HashRingEvenTest.class);
 
         // Special performance testing file
         // clientSuite.addTestSuite(PerformanceTest.class); // ??
